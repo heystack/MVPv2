dc1ad7eca94db949c24136d2260cc5e514acbda6
StkUp <nycbrown@gmail.com>
2012-01-25 23:24:22 -0500

    Colorful hc

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,7 +1,7 @@
 # Application constants
 LOWEST_COLOR = '#10ae11' # '#999' '#8cb941' '#89a54e'
-ALL_NEIGHBORS_COLOR = '#92A8CD'
-YOU_COLOR = '#ff8400' # '#0088ce' '#4572A7'
+ALL_NEIGHBORS_COLOR = '#999' # '#92A8CD'
+YOU_COLOR = '#0088ce' # '#4572A7'
 BIGGEST_COLOR = '#ff8400' # '#ff6e2c' db843d
 
 TIPPING_POINT = 50
