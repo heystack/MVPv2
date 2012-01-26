7c9f20c9ff58e000f6ca4df0fe9f340aad1766d2
StkUp <nycbrown@gmail.com>
2012-01-25 23:17:56 -0500

    Colorful hc

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,7 +1,7 @@
 # Application constants
-LOWEST_COLOR = '#999' # '#10ae11' '#8cb941' '#89a54e'
-ALL_NEIGHBORS_COLOR = '#999' # '#92A8CD'
+LOWEST_COLOR = '#10ae11' # '#999' '#8cb941' '#89a54e'
+ALL_NEIGHBORS_COLOR = '#92A8CD'
 YOU_COLOR = '#ff8400' # '#0088ce' '#4572A7'
-BIGGEST_COLOR = '#999' # '#ff8400' '#ff6e2c' db843d
+BIGGEST_COLOR = '#ff8400' # '#ff6e2c' db843d
 
 TIPPING_POINT = 50
