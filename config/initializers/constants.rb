6bf8a8559413d3d08f6367a968b797fe89b36c2b
StkUp <nycbrown@gmail.com>
2012-01-27 17:40:55 -0500

    Switch Lowest and YOU colors

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,7 +1,7 @@
 # Application constants
-LOWEST_COLOR = '#0088ce' # '#10ae11' # '#999' '#8cb941' '#89a54e'
+LOWEST_COLOR = '#ffdd55' # '#10ae11' # '#999' '#8cb941' '#89a54e'
 ALL_NEIGHBORS_COLOR = '#999' # '#92A8CD'
-YOU_COLOR = '#ffdd55' # '#4572A7'
+YOU_COLOR = '#0088ce' # '#4572A7'
 BIGGEST_COLOR = '#ff8400' # '#ff6e2c' db843d
 
 TIPPING_POINT = 50
