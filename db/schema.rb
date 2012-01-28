22cf64a20e74b5db9ab06195139d6d2b4d89f713
StkUp <nycbrown@gmail.com>
2012-01-27 23:45:10 -0500

    rails generate session_migration, change to active_record_store

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120112162717) do
+ActiveRecord::Schema.define(:version => 20120128044122) do
 
   create_table "comments", :force => true do |t|
     t.string   "content"
@@ -29,6 +29,16 @@ ActiveRecord::Schema.define(:version => 20120112162717) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "sessions", :force => true do |t|
+    t.string   "session_id", :null => false
+    t.text     "data"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
+  add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
 
   create_table "stacks", :force => true do |t|
     t.string   "attr_biggest_desc"
