35d5568dfe60d76e8908a5f5295c663ece5380c4
StkUp <nycbrown@gmail.com>
2012-01-28 22:39:46 -0500

    Working IE cookie issue

--- app/controllers/responses_controller.rb
+++ app/controllers/responses_controller.rb
@@ -46,7 +46,7 @@ class ResponsesController < ApplicationController
 
   def create
     @stack = Stack.find_by_id(session[:stack])
-    flash[:notice] = "session[:stack] = " + session[:stack].to_s
+    flash.now[:notice] = "session[:stack] = " + session[:stack].to_s
     redirect_to new_stack_response_path(1) and return
     @response = @stack.responses.build(params[:response])
     @user = User.find_by_id(@response.user_id)
