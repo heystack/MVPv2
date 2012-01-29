16f3b412059f714151144319b228d9fcb4c952ca
StkUp <nycbrown@gmail.com>
2012-01-28 23:08:26 -0500

    Working IE cookie issue

--- app/controllers/responses_controller.rb
+++ app/controllers/responses_controller.rb
@@ -1,17 +1,6 @@
 class ResponsesController < ApplicationController
 
   def new
-    # Replication of first part of Sessions#Create - probably a better way to do this
-    user = current_user
-    if user.nil?
-      @user = User.new
-      if @user.save
-        sign_in @user
-      end
-    else
-      sign_in user
-    end
-    # end Sessions#Create snippet
     if !session[:stack]
       @stack = Stack.first
       session[:stack] = @stack.id
