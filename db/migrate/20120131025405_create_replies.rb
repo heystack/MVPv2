77146b88fd8dc8a6d72cba5b178238f886ffb552
StkUp <nycbrown@gmail.com>
2012-01-31 02:06:59 -0500

    Increase comment size to 1000 char

--- db/migrate/20120131025405_create_replies.rb
+++ db/migrate/20120131025405_create_replies.rb
@@ -1,7 +1,7 @@
 class CreateReplies < ActiveRecord::Migration
   def change
     create_table :replies do |t|
-      t.string :content
+      t.string :content, :limit => 1000
       t.integer :comment_id
       t.integer :user_id
 
