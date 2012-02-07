37cf8d932b0976e83db56f63f97df6ccf303198d
StkUp <nycbrown@gmail.com>
2012-02-06 22:57:39 -0500

    RM suggestions

--- app/controllers/stacks_controller.rb
+++ app/controllers/stacks_controller.rb
@@ -133,7 +133,6 @@ class StacksController < ApplicationController
       if @user_rank == "lowest"
         # Not sure what would cause a zero, but seems to be happening, so check for it
         if session[:you] < 0.0001
-          flash[:error] = "Response must be greater than zero."
           @mult = 0.0
         else
           @mult = ( @lowest_amt / session[:you] ).round
