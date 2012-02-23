4418c5181b5e46c525ddd9393bfa272f732e554a
StkUp <nycbrown@gmail.com>
2012-02-22 23:16:48 -0500

    Remove blank select entries in madlib

--- app/models/stack.rb
+++ app/models/stack.rb
@@ -37,12 +37,12 @@ class Stack < ActiveRecord::Base
     end
     if qual.nil?
       if self.responses.count > 5
-        ( val > self.responses.maximum('value') * 2 ) ||
+        ( val > self.responses.maximum('value') * 1.5 ) ||
         ( val < self.responses.minimum('value') * 0.5 )
       end
     else
       if self.responses.where('qualifier1 = ?', qual).count > 5
-        ( val > self.responses.where('qualifier1 = ?', qual).maximum('value') * 2 ) ||
+        ( val > self.responses.where('qualifier1 = ?', qual).maximum('value') * 1.5 ) ||
         ( val < self.responses.where('qualifier1 = ?', qual).minimum('value') * 0.5 )
       end
     end
