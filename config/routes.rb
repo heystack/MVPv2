730d15ccc5ba3e69ef2d66fa7b92390e90d3a495
StkUp <nycbrown@gmail.com>
2012-03-26 12:38:41 -0400

    fixing listserv route

--- config/routes.rb
+++ config/routes.rb
@@ -61,6 +61,7 @@ Mvpv2::Application.routes.draw do
   match '/send_comment',        :to => 'mvp#send_comment'
   match '/stack_request',       :to => 'mvp#stack_request'
   match '/howitworks',          :to => 'mvp#howitworks'
+  match '/stacks/5.',          :to => '/stacks/5'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
